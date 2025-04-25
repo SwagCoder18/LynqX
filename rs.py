e78d923ba57fd4d2496f64ca81acc1feba31110f
SwagCoder18 <swagcoders18@gmail.com>
2025-04-25 17:03:40 +0600

    Update rs.py

--- rs.py
+++ rs.py
@@ -16,7 +16,7 @@ from pydantic import BaseModel
 
 app = FastAPI()
 ROOMS = {}  # {room_id: {"clients": [queue], "messages": [message]}}
-CLEANUP_INTERVAL = 2  # seconds
+CLEANUP_INTERVAL = 5  # seconds
 MAX_HISTORY = 50  # Max messages to store per room
 
 class Message(BaseModel):
